df20a700694442fa2df30136173a261d907bb41a
Christopher Kumm <chrislkumm@protonmail.com>
2022-04-28 14:57:48 +0300

    Fix type errors in `TextInput` component

--- components/TextInput/TextInput.tsx
+++ components/TextInput/TextInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FieldConfig, useField } from 'formik'
+import { FieldConfig, FieldHookConfig, useField } from 'formik'
 import styles from './TextInput.module.scss'
 
 type TextInputProps = {
@@ -7,16 +7,17 @@ type TextInputProps = {
   label: string
   id: string
   className?: string
-} & FieldConfig
+} & FieldHookConfig<string>
 
 export const TextInput = React.forwardRef(function TextInputInner(
   props: TextInputProps,
   ref: React.ForwardedRef<HTMLDivElement>
 ) {
+  const { variant, label, id, className, ...fieldProps } = props
   // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
   // which we can spread on <input>. We can use field meta to show an error
   // message if the field is invalid and it has been touched (i.e. visited)
-  const [field, meta] = useField(props)
+  const [field, meta] = useField(fieldProps)
   const invalid = meta.touched && meta.error
 
   let inputElement
@@ -24,18 +25,16 @@ export const TextInput = React.forwardRef(function TextInputInner(
   if (props.variant === 'input') {
     inputElement = (
       <input
-        className={`${styles.input} ${invalid && styles.inputInvalid}`}
+        className={`${styles.input} ${invalid ? styles.inputInvalid : ''}`}
         {...field}
-        {...props}
       />
     )
   } else if (props.variant === 'textarea') {
     inputElement = (
       <textarea
         className={`${styles.textarea}
-        ${invalid && styles.textareaInvalid}`}
+        ${invalid ? styles.textareaInvalid : ''}`}
         {...field}
-        {...props}
       />
     )
   }
@@ -43,7 +42,7 @@ export const TextInput = React.forwardRef(function TextInputInner(
   return (
     <div ref={ref} className={`${styles.base} ${props.className}`}>
       <label
-        className={`${styles.label} ${invalid && styles.invalidText}`}
+        className={`${styles.label} ${invalid ? styles.invalidText : ''}`}
         htmlFor={props.id || props.name}
       >
         {props.label}
