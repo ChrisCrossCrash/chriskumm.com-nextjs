6213f6d14a287f66af430227ad4eb9f4e2e4eb82
Christopher Kumm <chrislkumm@protonmail.com>
2022-04-29 20:30:43 +0300

    Fix contact form conditional rendering
    The old version didn't correctly use the
    `react-intersection-observer` API.

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,16 +1,17 @@
 import { SectionHeading } from '../SectionHeading/SectionHeading'
 import { ContactForm } from '../ContactForm/ContactForm'
 import styles from './Contact.module.scss'
-import { InView } from 'react-intersection-observer'
+import { useInView } from 'react-intersection-observer'
 
-export const Contact = () => (
-  <div id='contact' className={styles.base}>
-    <SectionHeading>Contact</SectionHeading>
-    <div className={styles.formContainer}>
-      {/* Only render the form when it is scrolled in to view */}
-      <InView>
-        <ContactForm />
-      </InView>
+export const Contact = () => {
+  const [ref, inView] = useInView()
+  return (
+    <div id='contact' className={styles.base}>
+      <SectionHeading>Contact</SectionHeading>
+      <div className={styles.formContainer} ref={ref}>
+        {/* Only render the form when it is scrolled in to view */}
+        {inView && <ContactForm />}
+      </div>
     </div>
-  </div>
-)
+  )
+}
