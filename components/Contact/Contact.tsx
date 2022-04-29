58450b8aead045fb3e5a9fbbc620d80f2ae1f32a
Christopher Kumm <chrislkumm@protonmail.com>
2022-04-29 20:23:34 +0300

    Only render the contact form in viewport
    Loading the contact form only when its in viewport prevents unnecessary
    loading of the reCaptcha element.

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,12 +1,16 @@
 import { SectionHeading } from '../SectionHeading/SectionHeading'
 import { ContactForm } from '../ContactForm/ContactForm'
 import styles from './Contact.module.scss'
+import { InView } from 'react-intersection-observer'
 
 export const Contact = () => (
   <div id='contact' className={styles.base}>
     <SectionHeading>Contact</SectionHeading>
     <div className={styles.formContainer}>
-      <ContactForm />
+      {/* Only render the form when it is scrolled in to view */}
+      <InView>
+        <ContactForm />
+      </InView>
     </div>
   </div>
 )
