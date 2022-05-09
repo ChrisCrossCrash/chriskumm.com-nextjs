6ab6adaa07138ac96031ed6d36700c972dd889df
Christopher Kumm <chrislkumm@protonmail.com>
2022-05-09 17:02:40 +0300

    Add favicons

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,34 @@ const Home: NextPage = () => (
     <Head>
       <title>ChrisKumm.com</title>
       <meta name='description' content='Professional Full-Stack Developer' />
-      <link rel='icon' href='/favicon.ico' />
+
+      {/* https://realfavicongenerator.net/ */}
+
+      <link
+        rel='apple-touch-icon'
+        sizes='180x180'
+        href='/favicons/apple-touch-icon.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='32x32'
+        href='/favicons/favicon-32x32.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='16x16'
+        href='/favicons/favicon-16x16.png'
+      />
+      <link rel='manifest' href='/site.webmanifest' />
+      <link
+        rel='mask-icon'
+        href='/favicons/safari-pinned-tab.svg'
+        color='#5bbad5'
+      />
+      <meta name='msapplication-TileColor' content='#2b5797' />
+      <meta name='theme-color' content='#ffffff' />
     </Head>
 
     <Hero />
