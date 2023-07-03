cbde6053e961233ff7915b5284421b87edf25ffb
Christopher Kumm <chrislkumm@protonmail.com>
2023-07-03 14:16:47 +0300

    Add JSDoc comment

--- cypress/cypressCustomTypes.d.ts
+++ cypress/cypressCustomTypes.d.ts
@@ -1,6 +1,14 @@
 declare namespace Cypress {
   interface Chainable<Subject = any> {
-    // TODO: Add a JSDoc comment here.
+    /**
+     * Gets an input element by its associated label.
+     *
+     * @param {string} label - The text content of the label.
+     * @returns Cypress object for chaining.
+     *
+     * @example
+     * cy.getInputByLabel('Email').type('test@example.com');
+     */
     getInputByLabel(value: string): Chainable<Subject>
   }
 }
