ba3932e31d2ce4ee072cbf27006b52e12956493c
Christopher Kumm <chrislkumm@protonmail.com>
2023-10-19 12:41:46 -0500

    Write more tests for `Chat`

--- components/Chat/Chat.cy.tsx
+++ components/Chat/Chat.cy.tsx
@@ -35,4 +35,58 @@ describe('<Chat />', () => {
       cy.contains('Hello from the bot!')
     })
   })
+
+  it('Can submit by pressing enter', () => {
+    cy.get('textarea').type('User message{enter}')
+    // Assert 'p' so that we don't just query the textarea
+    cy.contains('p', 'User message')
+  })
+
+  it('Can submit by clicking the button', () => {
+    cy.get('textarea').type('User message')
+    cy.get('input[type="submit"]').click()
+    // Assert 'p' so that we don't just query the textarea
+    cy.contains('p', 'User message')
+  })
+
+  it('Clears the text area after sending a message', () => {
+    cy.get('textarea').type('User message{enter}')
+    cy.get('textarea').should('have.value', '')
+  })
+
+  it('Renders markdown links', () => {
+    cy.get('textarea').type('https://www.example.com{enter}')
+    cy.contains('a', 'https://www.example.com')
+  })
+
+  it('Renders markdown tables', () => {
+    cy.get('textarea').type(
+      '| Header 1 | Header 2 |{shift+enter}| -------- | -------- |{shift+enter}| Cell 1 | Cell 2 |{enter}',
+    )
+    cy.contains('table', 'Header 1')
+    cy.contains('table', 'Header 2')
+    cy.contains('table', 'Cell 1')
+    cy.contains('table', 'Cell 2')
+  })
+
+  it('Renders markdown code blocks', () => {
+    cy.get('textarea').type(
+      '```{shift+enter}const foo = "bar"{shift+enter}```{enter}',
+    )
+    cy.contains('pre', 'const foo = "bar"')
+  })
+
+  it.only('Auto-scrolls to the bottom of the chat when a new message appears', () => {
+    // Write several messages to the chat to make it scrollable
+    cy.get('textarea').type('First User message{enter}')
+    cy.get('textarea').type('User message{enter}')
+    cy.get('textarea').type('User message{enter}')
+    cy.get('textarea').type('Last user message{enter}')
+
+    // The first user message should not be visible
+    cy.contains('First User message').should('not.be.visible')
+
+    // The last user message should be visible
+    cy.contains('Last user message').should('be.visible')
+  })
 })
