4761d33afda8d8065f4b45aedef7857eb07d95c8
Christopher Kumm <chrislkumm@protonmail.com>
2023-10-19 10:58:42 -0500

    Add a few `Chat` tests

--- components/Chat/Chat.cy.tsx
+++ components/Chat/Chat.cy.tsx
@@ -2,8 +2,33 @@ import React from 'react'
 import Chat from './Chat'
 
 describe('<Chat />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
+  beforeEach(() => {
     cy.mount(<Chat />)
+
+    // Mock the OpenAI API call
+    cy.intercept('POST', '/api/chat-api', {
+      statusCode: 200,
+      body: {
+        result: 'Hello from the bot!',
+      },
+    }).as('apiCheck')
+  })
+
+  it('renders the chat interface', () => {
+    cy.get('textarea').should('exist')
+    cy.get('input[type="submit"]').should('exist')
+  })
+
+  it('sends a user message', () => {
+    cy.get('textarea').type('User message{enter}')
+    // Get the `.message` element with the user's "User message" text.
+    cy.contains('User message')
+  })
+
+  it('receives an assistant message', () => {
+    cy.get('textarea').type('Hello!{enter}')
+    cy.wait('@apiCheck').then(() => {
+      cy.contains('Hello from the bot!')
+    })
   })
 })
